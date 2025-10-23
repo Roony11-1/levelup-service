6ffe8996a83db98ec202790ce7620ba2ca099524
Roony11-1 <eltheasasin@gmail.com>
2025-10-23 19:07:38 -0300

    Commit Inicial

---
+++ src/main/java/com/patitofeliz/levelup_service/LevelupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patitofeliz.levelup_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patitofeliz/levelup_service/LevelupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patitofeliz.levelup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
