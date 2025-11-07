f9cc98c8061ce2823faf677d3d60330f19b97468
Roony11-1 <eltheasasin@gmail.com>
2025-11-07 12:31:12 -0300

    adios normalizacion!

--- src/main/java/com/patitofeliz/levelup_service/model/usuario/Usuario.java
+++ src/main/java/com/patitofeliz/levelup_service/model/usuario/Usuario.java
@@ -1,16 +1,11 @@
 package com.patitofeliz.levelup_service.model.usuario;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.patitofeliz.levelup_service.model.ubicacion.Comuna;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -26,10 +21,9 @@ public class Usuario
     private String contraseña;
     private String telefono;
 
-    @ManyToOne
-    @JoinColumn(name = "id_comuna")
-    private Comuna comuna;
-
+    private String comuna;
+    private String region;
+    
     private String tipo;
     private String profilePhoto;
 }
