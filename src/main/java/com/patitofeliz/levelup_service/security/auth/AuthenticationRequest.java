aef0a55ae5cb6a7cf696407153c2b2f6010123ed
Neytan2214 <ney.silva@duocuc.cl>
2025-11-25 09:20:55 -0300

    ---auth a medias---

---
+++ src/main/java/com/patitofeliz/levelup_service/security/auth/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.patitofeliz.levelup_service.security.auth;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AuthenticationRequest {
+
+    private String email;
+    String contraseña;
+
+}
---
+++ src/main/java/com/patitofeliz/levelup_service/security/auth/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.patitofeliz.levelup_service.security.auth;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RegisterRequest {
+    private int id;
+
+    private String nombreUsuario;
+    private String email;
+    private String contraseña;
+    private String telefono;
+
+    private String comuna;
+    private String region;
+    
+    private String tipo;
+    
+}
